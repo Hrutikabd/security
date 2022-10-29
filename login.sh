b64f4057c20e63769a56eb86504abaaebb17f7ef
Hrutikabd <101612719+Hrutikabd@users.noreply.github.com>
2022-10-29 08:07:22 -0400

    Add files via upload
    login security

---
+++ login.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+to="email"
+echo " enter password"
+read a
+if [ $a != 1234 ]
+then
+	echo "try again"
+	echo " enter password again"
+	read b
+	if [ $b != 1234 ] 
+	then
+		echo "alert bro "
+		echo `fswebcam -r 640x480 --jpeg 85 -D 1 web-cam-shot.jpg`
+		echo `poweroff`
+
+	else
+		echo "welcome"
+	fi
+else
+	echo "welcome"
+fi
